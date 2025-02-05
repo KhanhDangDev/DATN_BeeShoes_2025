74643625fed74d4084f82d2a77bb4bd16951efa5
tu-ndfpt <tudnph48063@fpt.edu.vn>
2025-02-05 23:00:44 +0700

    sua route

--- server/routes/api.php
+++ server/routes/api.php
@@ -1,8 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\ProductController;
 
 Route::middleware(['api'])->group(function () {
     require __DIR__ . '/api/san-pham/api.php';
