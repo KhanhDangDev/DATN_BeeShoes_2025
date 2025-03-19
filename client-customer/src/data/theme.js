4a282a3363f2d926ec8d9ab6a06ce404ba628d64
chuanhngoc <chuthieugia8102003@gmail.com>
2025-03-19 15:02:15 +0700

    tạo project bên mua hàng

---
+++ client-customer/src/data/theme.js
@@ -0,0 +1,14 @@
+export default {
+  name: 'BeeShoes Shop',
+  fullName: 'BeeShoes Shop Shoes4',
+  url: 'https://themeforest.net/item/stroyka-tools-store-react-ecommerce-template/23909258',
+  author: {
+    name: 'Kos',
+    profile_url: 'https://themeforest.net/user/kos9',
+  },
+  contacts: {
+    address: 'Fpoly Trịnh Văn Bô, Nam Từ Liêm, Hà Nội',
+    email: 'BeeShoes@example.com',
+    phone: '(800) 060-0730',
+  },
+};
